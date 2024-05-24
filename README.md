1106a98072729afa5215965b228a4445a6c0ba15
Garretonzo <garretseppala16@gmail.com>
2024-05-23 21:50:21 -0700

    create_new_plugin contents

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Example
-An example greeter plugin
+# TonzoAPI
+Simple HTTP Server API  
+Endpoints:
+* http://localhost:5252/api/inv/
+* http://localhost:5252/api/stats/
--- src/test/java/com/tonzo/api/TonzoApiPluginTest.java
+++ src/test/java/com/tonzo/api/TonzoApiPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.tonzo.api;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TonzoApiPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TonzoApiPlugin.class);
 		RuneLite.main(args);
 	}
 }
